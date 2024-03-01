2f82959afe44b3712e878e5aff8ecd81d8d028ec
Ivan Aguilar <ivanaguiglez@gmail.com>
2024-03-01 11:05:27 +0100

    Initial commit

---
+++ holaMundo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hola Mundo</title>
+</head>
+<body>
+  <div>
+    <?php
+    $nombre="Jose";
+    $apellido="Pérez";
+    $edad=37;
+    echo "<h1>Hola $nombre $apellido</h1>
+    <h2>Edad $edad </h2>"
+    ?>
+  </div>
+</body>
+</html
